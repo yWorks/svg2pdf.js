97fb65c90ce6f07217e3ffb229a7953ed02f9aef
Lukas Holländer <lukas.hollaender@yworks.com>
2018-02-01 00:00:21 +0100

    migrate most important tests to new format

--- tests/runTests.js
+++ tests/runTests.js
@@ -1,5 +1,17 @@
 const tests = [
-  "fill-and-stroke-opacity"
+    "display-none-and-visibility-inheritance",
+    "fill-and-stroke-inheritance",
+    "fill-and-stroke-opacity",
+    "gradient-default-coordinates",
+    "gradients-and-patterns-mixed",
+    "line-default-coordinates",
+    "markers",
+    "opacity-and-rgba",
+    "path-arg-support",
+    "strokes-and-bounding-boxes",
+    "text-placement",
+    "transparent-pngs",
+    "zero-width-strokes"
 ];
 
 for (let name of tests) {
