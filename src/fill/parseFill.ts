100fd9d369daed065dbd30d9cec9edcc32de5719
Lukas Holländer <lukas.hollaender@yworks.com>
2021-07-20 12:20:41 +0200

    fix gradients with zero or one stop (#185)
    fix #165

--- src/fill/parseFill.ts
+++ src/fill/parseFill.ts
@@ -9,6 +9,7 @@ import { PatternFill } from './PatternFill'
 import { ColorFill } from './ColorFill'
 import { RGBColor } from '../utils/rgbcolor'
 import { parseColor } from '../utils/parsing'
+import { Gradient } from '../nodes/gradient'
 
 export function parseFill(fill: string, context: Context): Fill | null {
   const url = iriReference.exec(fill)
@@ -16,7 +17,7 @@ export function parseFill(fill: string, context: Context): Fill | null {
     const fillUrl = url[1]
     const fillNode = context.refsHandler.get(fillUrl)
     if (fillNode && (fillNode instanceof LinearGradient || fillNode instanceof RadialGradient)) {
-      return new GradientFill(fillUrl, fillNode)
+      return getGradientFill(fillUrl, fillNode, context)
     } else if (fillNode && fillNode instanceof Pattern) {
       return new PatternFill(fillUrl, fillNode)
     } else {
@@ -34,4 +35,26 @@ export function parseFill(fill: string, context: Context): Fill | null {
       return null
     }
   }
+}
+
+function getGradientFill(fillUrl: string, gradient: Gradient, context: Context): Fill | null {
+  // "It is necessary that at least two stops are defined to have a gradient effect. If no stops are
+  // defined, then painting shall occur as if 'none' were specified as the paint style. If one stop
+  // is defined, then paint with the solid color fill using the color defined for that gradient
+  // stop."
+  const stops = gradient.getStops(context.styleSheets)
+  if (stops.length === 0) {
+    return null
+  }
+  if (stops.length === 1) {
+    const stopColor = stops[0].color
+    const rgbColor = new RGBColor()
+    rgbColor.ok = true
+    rgbColor.r = stopColor[0]
+    rgbColor.g = stopColor[1]
+    rgbColor.b = stopColor[2]
+    rgbColor.a = stops[0].opacity
+    return new ColorFill(rgbColor)
+  }
+  return new GradientFill(fillUrl, gradient)
 }
