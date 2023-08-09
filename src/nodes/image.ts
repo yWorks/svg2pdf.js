95bfe53fd7c0a061a347327d7d31c3ec036a1944
Anna-Lena Lumpp <anna-lena.lumpp@yworks.com>
2023-08-09 14:27:41 +0200

    fixed requested changes

--- src/nodes/image.ts
+++ src/nodes/image.ts
@@ -10,7 +10,7 @@ import { Matrix } from 'jspdf'
 import { Viewport } from '../context/viewport'
 
 // groups: 1: mime-type (+ charset), 2: mime-type (w/o charset), 3: charset, 4: base64?, 5: body
-export const dataUriRegex = /^\s*data:(([^/,;]+\/[^/,;]+)(?:;([^,;=]+=[^,;=]+))?)?(?:;(base64))?,((.|\s)*)$/i
+export const dataUriRegex = /^\s*data:(([^/,;]+\/[^/,;]+)(?:;([^,;=]+=[^,;=]+))?)?(?:;(base64))?,(?:(.|\s)*)$/i
 export class ImageNode extends GraphicsNode {
   private readonly imageLoadingPromise: Promise<{ data: string; format: string }> | null = null
   private readonly imageUrl: string | null
@@ -117,8 +117,8 @@ export class ImageNode extends GraphicsNode {
       format = mimeTypeParts[1]
 
       data = match[5]
-      data = data.replace(/\s/g, '')
       if (match[4] === 'base64') {
+        data = data.replace(/\s/g, '')
         data = atob(data)
       } else {
         data = decodeURIComponent(data)
